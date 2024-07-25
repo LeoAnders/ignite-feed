57493563ab7b7838a4be88eafe63b70e96f1befa
LeoAnders <leonardoanders15@gmail.com>
2024-07-25 19:34:28 -0300

    Estrutura da aplicação - Componentes `Comment` e suas respectivas estilizações

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/115679546?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Leonardo Anders</strong>
+              <time title='06 de junho as 09:15h' dateTime="2023-06-16 09:15:50">Cerca de uma 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post(){
@@ -27,12 +28,17 @@ export function Post(){
 
         <textarea placeholder='Deixe seu comentário'></textarea>
 
-      <footer>
-        <button type='submit'>Publicar</button>
-      </footer>
-        
-
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+      </div>
+
     </article>
   )
 }
